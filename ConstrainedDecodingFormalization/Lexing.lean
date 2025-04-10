fff2de74847e2bc9d13f2aba0839c68249d84c02
Arnav Dandu <arnavdandu@gmail.com>
2025-04-10 12:51:57 -0700

    started PartialLexer

--- ConstrainedDecodingFormalization/Lexing.lean
+++ ConstrainedDecodingFormalization/Lexing.lean
@@ -10,20 +10,22 @@ import Std.Data.HashSet
 
 universe u v w
 
+#check Language
+
 section Symbols
 variable
   [BEq α] [BEq σ]
   {α : Type u} {β : Type u} {σ : Type v}
-  (EOS : α)
+  (EOS : List α)
 
-structure FSA (α) (σ) where
+structure FSA (α σ) where
   alph : α
   states : List σ
   start : List σ
   step : σ → α → List σ
   accept : List σ
 
-structure FST (α) (β) (σ) where
+structure FST (α β σ) where
   alph : α
   oalph : β
   states : List σ
@@ -34,10 +36,38 @@ structure FST (α) (β) (σ) where
 
 open Std
 
-instance [BEq σ] [DecidableEq σ] : Coe (FSA α σ) (NFA α σ) := ⟨λ fsa => {
+instance [DecidableEq σ] : Coe (FSA α σ) (NFA α σ) := ⟨λ fsa => {
   start := (FSA.start fsa).toFinset
   step := λ q a => (FSA.step fsa q a).toFinset
   accept := (FSA.accept fsa).toFinset
 }⟩
+
+structure LexerSpec (α β σ) where
+  automaton : FSA α σ
+  term_sym : β
+
+-- A recognizer for a token: returns true if the input is a valid token
+def isToken : List α → Option β := sorry
+
+-- A predicate for prefix of any token
+def isPrefix : List α → Prop := sorry
+
+noncomputable def NFA.prefixAccepts {α σ} (nfa : NFA α σ) (xs : List α) : Prop :=
+  ∃ ys, NFA.accepts nfa (xs ++ ys)
+
+noncomputable def PartialLexer [DecidableEq σ] (specs : List (LexerSpec α β σ)) (EOS : List α) : List α → Option (List β × List α) :=
+  letI := Classical.propDecidable
+
+  let isToken (xs : List α) : Option β :=
+    specs.findSome? fun s =>
+      let nfa : NFA α σ := s.automaton
+      if NFA.accepts nfa xs then some s.term_sym else none
+
+  let isPrefix (xs : List α) : Prop :=
+    specs.any fun s =>
+      let nfa : NFA α σ := s.automaton
+      NFA.prefixAccepts nfa xs
+
+  sorry
 
 end Symbols
