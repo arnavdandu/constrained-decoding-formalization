33d79c743cf4a1b1b8b8904239a84e79e4863b0f
Arnav Dandu <arnavdandu@gmail.com>
2025-04-24 14:20:15 -0700

    switched beta to gamma

--- ConstrainedDecodingFormalization/Lexing.lean
+++ ConstrainedDecodingFormalization/Lexing.lean
@@ -1,141 +1,142 @@
 import Mathlib.Computability.NFA
-import Mathlib.Computability.DFA
-import Mathlib.Computability.RegularExpressions
-import Mathlib.Data.Set.Defs
-import Mathlib.Data.Set.Basic
-import Mathlib.Data.List.Basic
-import Mathlib.Data.Finset.Basic
-import Std.Data.HashSet
---import Mathlib.Data.Set.Finite
+ import Mathlib.Computability.DFA
+ import Mathlib.Computability.RegularExpressions
+ import Mathlib.Data.Set.Defs
+ import Mathlib.Data.Set.Basic
+ import Mathlib.Data.List.Basic
+ import Mathlib.Data.Finset.Basic
+ import Std.Data.HashSet
+ --import Mathlib.Data.Set.Finite
 
-open Classical List
+ open Classical List
 
-universe u v w
+ universe u v w
 
-#check Language
+ #check Language
 
-section Symbols
-variable
-  {α : Type u} {Γ : Type u} {σ : Type v}
-  [BEq α] [BEq σ] [BEq Γ]
-  [DecidableEq α] [DecidableEq σ] [DecidableEq Γ]
-  [Inhabited α] [Inhabited Γ]
-  (EOS : α)
+ section Symbols
+ variable
+   [BEq α] [BEq σ] [BEq Γ]
+   {α : Type u} {Γ : Type u} {σ : Type v}
+   [DecidableEq α] [DecidableEq σ] [DecidableEq Γ]
+   [Inhabited α] [Inhabited Γ]
+   (EOS : α)
 
-structure FSA (α σ) where
-  alph : List α
-  states : List σ
-  start : List σ
-  step : σ → α → List σ
-  accept : List σ
+ structure FSA (α σ) where
+   alph : List α
+   states : List σ
+   start : List σ
+   step : σ → α → List σ
+   accept : List σ
 
-def FSA.transitions (fsa : FSA α σ) : List (σ × α × List σ) :=
-  fsa.states.flatMap (fun q =>
-    (fsa.alph.map (fun c =>
-        (q, c, fsa.step q c)
-       )
-    )
-  )
+ def FSA.transitions (fsa : FSA α σ) : List (σ × α × List σ) :=
+   fsa.states.flatMap (fun q =>
+     (fsa.alph.map (fun c =>
+         (q, c, fsa.step q c)
+        )
+     )
+   )
 
-def FSA.mkStep (transitions : List (σ × α × List σ)) : σ → α → List σ :=
-  fun s a =>
-    transitions.filterMap (fun (s', a', ts) =>
-      if s = s' && a = a' then some ts else none
-    )
-    |> List.flatten
-
-
-structure FST (α Γ σ) where
-  alph : List α
-  oalph : List Γ
-  states : List σ
-  start : List σ
-  step : σ → α → (List σ × List Γ)
-  accept : List σ
-
-def FST.transitions (fst : FST α Γ σ) : List (σ × α × (List σ × List Γ)) :=
-  fst.states.flatMap (fun q =>
-    (fst.alph.map (fun c =>
-        (q, c, fst.step q c)
-       )
-    )
-  )
-
-def FST.mkStep (transitions : List (σ × α × (List σ × Γ))) : σ → α → (List σ × Γ) :=
-  fun s a =>
-    transitions.find? (fun (s', a', _) => s = s' && a = a')
-    |>.map (fun (_, _, ts) => ts)
-    |>.getD ([], default)
-
-instance [DecidableEq σ] : Coe (FSA α σ) (NFA α σ) := ⟨fun fsa => {
-  start := (FSA.start fsa).toFinset
-  step := fun q a => (FSA.step fsa q a).toFinset
-  accept := (FSA.accept fsa).toFinset
-}⟩
-
-structure LexerSpecA (α Γ σ) where
-  automaton : FSA α σ
-  term_sym : Γ
-
--- A recognizer for a token: returns true if the input is a valid token
-noncomputable def isToken (specs : List (LexerSpecA α Γ σ)) (xs : List α) : Option Γ :=
-  specs.findSome? fun s =>
-    let nfa : NFA α σ := s.automaton
-    if (NFA.accepts nfa) xs then s.term_sym else none
-
--- A predicate for prefix of any token
-def isPrefix (specs : List (LexerSpecA α Γ σ)) (xs : List α) : Prop :=
-  specs.any fun s =>
-    let nfa : NFA α σ := s.automaton
-    ∃ ys, NFA.accepts nfa (xs ++ ys)
-
-inductive LexRel (specs : List (LexerSpecA α Γ σ)) :
-    List α → List Γ → List α → Prop
-  | empty :
-      LexRel specs [] [] []
-
-  -- (T₁...Tₖ T^j, ε) if c = EOS and wr ∈ L(A^j)
-  | done {wr tj} :
-      isToken specs wr = some tj →
-      LexRel specs wr [tj] []
-
-  -- (T₁...Tₖ, wrc) if c ≠ EOS and wrc ∈ L_prefix(A^i) for some i
-  -- → (T₁...Tₖ T^j, c :: cs) if wr ∈ L(A^j) but wrc ∉ L_prefix(A^i) for all i.
-  | emit {wr c cs tj T} :
-      isToken specs wr = some tj →
-      ¬ isPrefix specs (wr ++ [c]) →
-      LexRel specs (c :: cs) T [] →
-      LexRel specs (wr ++ c :: cs) (tj :: T) wr
-
-  -- (T₁...Tₖ, wrc) if c ≠ EOS and wrc ∈ L_prefix(A^i) for some i.
-  | extend {wr c cs T} :
-      isPrefix specs (wr ++ [c]) →
-      LexRel specs cs T (wr ++ [c]) →
-      LexRel specs (c :: cs) T wr
-
-noncomputable def PartialLex (specs : List (LexerSpecA α Γ σ)) (w : List α) : Option (List Γ × List α) :=
-  if h : ∃ out : List Γ × List α, LexRel specs w out.1 out.2 then
-    some (choose h)
-  else none
-
-def BuildLexingFST (fsa : FSA α σ) (oalph : List α) (h : fsa.start.length = 1) : FST α α σ := Id.run do
-  let Q := fsa.states
-  let trans := fsa.transitions
-  let alph := fsa.alph
-  let q0 := fsa.start[0]
-
-  let F' := [q0]
-
-  let mut trans' : List (σ × α × (List σ × List α)) := trans.map (fun (q, c, q') => (q, c, (q', [])))
-  for q in Q do
-    for T in oalph do
-      if not (fsa.step q T).isEmpty then
-        for c in alph do
-          for q' in Q do
-            if ∃ q'' ∈ Q, q'' ∉ fsa.step q c ∧ q' ∈ (fsa.step q0 c) then
-              trans' := trans'.insert (q, c, ([q'], [T]))
-        trans' := trans'.insert (q, EOS, ([q0], [T, EOS]))
-  ⟨alph, oalph, Q, F', FST.mkStep trans', F'⟩
+ def FSA.mkStep (transitions : List (σ × α × List σ)) : σ → α → List σ :=
+   fun s a =>
+     transitions.filterMap (fun (s', a', ts) =>
+       if s = s' && a = a' then some ts else none
+     )
+     |> List.flatten
 
 
-end Symbols
+ structure FST (α Γ σ) where
+   alph : List α
+   oalph : List Γ
+   states : List σ
+   start : List σ
+   step : σ → α → (List σ × List Γ)
+   accept : List σ
+
+ def FST.transitions (fst : FST α Γ σ) : List (σ × α × (List σ × List Γ)) :=
+   fst.states.flatMap (fun q =>
+     (fst.alph.map (fun c =>
+         (q, c, fst.step q c)
+        )
+     )
+   )
+
+ def FST.mkStep (transitions : List (σ × α × (List σ × Γ))) : σ → α → (List σ × Γ) :=
+   fun s a =>
+     transitions.find? (fun (s', a', _) => s = s' && a = a')
+     |>.map (fun (_, _, ts) => ts)
+     |>.getD ([], default)
+
+ instance [DecidableEq σ] : Coe (FSA α σ) (NFA α σ) := ⟨fun fsa => {
+   start := (FSA.start fsa).toFinset
+   step := fun q a => (FSA.step fsa q a).toFinset
+   accept := (FSA.accept fsa).toFinset
+ }⟩
+
+ structure LexerSpec (α Γ σ) where
+   automaton : FSA α σ
+   term_sym : Γ
+
+ -- A recognizer for a token: returns true if the input is a valid token
+ noncomputable def isToken (specs : List (LexerSpec α Γ σ)) (xs : List α) : Option Γ :=
+   specs.findSome? fun s =>
+     let nfa : NFA α σ := s.automaton
+     if (NFA.accepts nfa) xs then s.term_sym else none
+
+ -- A predicate for prefix of any token
+ def isPrefix (specs : List (LexerSpec α Γ σ)) (xs : List α) : Prop :=
+   specs.any fun s =>
+     let nfa : NFA α σ := s.automaton
+     ∃ ys, NFA.accepts nfa (xs ++ ys)
+
+ inductive LexRel (specs : List (LexerSpec α Γ σ)) :
+     List α → List Γ → List α → Prop
+   | empty :
+       LexRel specs [] [] []
+
+   -- (T₁...Tₖ T^j, ε) if c = EOS and wr ∈ L(A^j)
+   | done {wr tj} :
+       isToken specs wr = some tj →
+       LexRel specs wr [tj] []
+
+   -- (T₁...Tₖ, wrc) if c ≠ EOS and wrc ∈ L_prefix(A^i) for some i
+   -- → (T₁...Tₖ T^j, c :: cs) if wr ∈ L(A^j) but wrc ∉ L_prefix(A^i) for all i.
+   | emit {wr c cs tj T} :
+       isToken specs wr = some tj →
+       ¬ isPrefix specs (wr ++ [c]) →
+       LexRel specs (c :: cs) T [] →
+       LexRel specs (wr ++ c :: cs) (tj :: T) wr
+
+   -- (T₁...Tₖ, wrc) if c ≠ EOS and wrc ∈ L_prefix(A^i) for some i.
+   | extend {wr c cs T} :
+       isPrefix specs (wr ++ [c]) →
+       LexRel specs cs T (wr ++ [c]) →
+       LexRel specs (c :: cs) T wr
+
+ noncomputable def PartialLex (specs : List (LexerSpec α Γ σ)) (w : List α) : Option (List Γ × List α) :=
+   if h : ∃ out : List Γ × List α, LexRel specs w out.1 out.2 then
+     some (choose h)
+   else none
+
+ def BuildLexingFST (fsa : FSA α σ) (oalph : List α) (h : fsa.start.length = 1) : FST α α σ := Id.run do
+   let Q := fsa.states
+   let trans := fsa.transitions
+   let alph := fsa.alph
+   let q0 := fsa.start[0]
+
+   let F' := [q0]
+
+   let mut trans' : List (σ × α × (List σ × List α)) := trans.map (fun (q, c, q') => (q, c, (q', [])))
+   for q in Q do
+     for T in oalph do
+       if not (fsa.step q T).isEmpty then
+         for c in alph do
+           for q' in Q do
+             if ∃ q'' ∈ Q, q'' ∉ fsa.step q c ∧ q' ∈ (fsa.step q0 c) then
+               trans' := trans'.insert (q, c, ([q'], [T]))
+         trans' := trans'.insert (q, EOS, ([q0], [T, EOS]))
+
+   ⟨alph, oalph, Q, F', FST.mkStep trans', F'⟩
+
+
+ end Symbols
